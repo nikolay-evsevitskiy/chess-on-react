f5ef167b69b94bd962e7921ebaf3942a6dbaa2f3
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-06-16 13:25:29 +0200

    set vertical roles for queen

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -14,7 +14,9 @@ export class Queen extends Figure {
     canMove(target: Cell): boolean {
         if (!super.canMove(target))
             return false;
-        return true
+        if (this.cell.isEmptyVertical(target))
+            return true
+        return false
     }
 
 }
